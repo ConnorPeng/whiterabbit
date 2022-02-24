12bab7ca69bc10e89924d4153ef57d63d7000b73
ConnorPeng <71860175+ConnorPeng@users.noreply.github.com>
2022-02-24 09:16:09 -1000

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,16 @@
+from gpiozero import Servo,AngularServo
+from time import sleep
+
+
+servo = AngularServo(17, min_angle = 0, max_angle = 180, min_pulse_width = 0.0006, max_pulse_width = 0.0024)
+
+try:
+    while True:
+        servo.min()
+        sleep(0.5)
+        servo.mid()
+        sleep(0.5)
+        servo.max()
+        sleep(0.5)
+except KeyboardInterrupt:
+    print("stop")
