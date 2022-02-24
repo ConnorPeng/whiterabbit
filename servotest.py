1191805f412def240b8aee450c8f1d7040024661
ConnorPeng <71860175+ConnorPeng@users.noreply.github.com>
2022-02-24 09:45:30 -1000

    Add files via upload

--- servotest.py
+++ servotest.py
@@ -1,9 +1,8 @@
-from gpiozero import Servo
+from gpiozero import Servo,AngularServo
 from time import sleep
 
-servo = Servo(25)
-val = -1
 
+servo = AngularServo(17, min_angle = 0, max_angle = 180, min_pulse_width = 0.0006, max_pulse_width = 0.0024)
 try:
     while True:
         
