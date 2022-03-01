0457b684f847748e35f8843a6e6925ce45375be8
ConnorPeng <71860175+ConnorPeng@users.noreply.github.com>
2022-03-01 09:03:03 -1000

    Add files via upload

--- test1.py
+++ test1.py
@@ -1,10 +1,14 @@
 from gpiozero import Servo,AngularServo
 from time import sleep
+from pygame import mixer
 
-
+mixer.init()
+sound = mixer.Sound("/home/pi/Downloads/sample3.wav")
+sound.play()
 
 
 servo = AngularServo(17, min_angle = 0, max_angle = 180, min_pulse_width = 0.0006, max_pulse_width = 0.0024)
+
 
 try:
     while True:
