311a805b6f1c9f0182d6b5b51c234673458c7257
ConnorPeng <pjh_jinghong@outlook.com>
2022-03-14 14:39:25 -0400

    Update rob_main.py

--- rob_main.py
+++ rob_main.py
@@ -1,4 +1,13 @@
 import RPi.GPIO as GPIO
+from gpiozero import Servo,AngularServo
+from time import sleep
+from pygame import mixer
+
+mixer.init()
+servo = AngularServo(17, min_angle = 0, max_angle = 180, min_pulse_width = 0.0006, max_pulse_width = 0.0024)
+
+
+
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -15,6 +24,20 @@ while True:
         GPIO.output(6, GPIO.LOW)
         GPIO.output(26, GPIO.LOW)
         print("test mode")
+        # outout sound
+        sound = mixer.Sound("/home/pi/Downloads/sample3.wav")
+        sound.play()
+        #servo movement
+        servo.min()
+        sleep(0.5)
+        servo.mid()
+        sleep(0.5)
+        servo.max()
+        sleep(0.5)
+        servo.mid()
+        sleep(0.5)
+        servo.min()
+
     elif GPIO.input(24) == GPIO.HIGH:
         GPIO.output(5, GPIO.LOW)
         GPIO.output(6, GPIO.HIGH)
